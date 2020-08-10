35a5af9daf4d1b913a15ff32c043006c8d34f834
primo9293 <42128291+primo9293@users.noreply.github.com>
2020-08-09 21:22:50 -0500

    Fecth

--- assets/index.js
+++ assets/index.js
@@ -3,8 +3,8 @@ import AutoPlay from './plugins/AutoPlay.js';
 
 const video = document.querySelector('video');
 const player = new MediaPlayer({
-    el: video,
-    //plugins: [new AutoPlay()]
+  el: video,
+  plugins: [new AutoPlay()],
 });
 
 const playButton = document.querySelector('#playButton');
@@ -12,9 +12,9 @@ playButton.onclick = () => player.togglePlay();
 
 const muteButton = document.querySelector('#muteButton');
 muteButton.onclick = () => {
-    if (player.media.muted) {
-        player.unmute();
-    } else {
-        player.mute();
-    }
+  if (player.media.muted) {
+    player.unmute();
+  } else {
+    player.mute();
+  }
 };
